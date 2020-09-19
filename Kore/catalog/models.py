f6832d4f8a05acfb7e96c6906b3ce87b1d014d4c
Sagnik Anupam <sagnikanupam@gmail.com>
2020-09-19 22:48:48 +0530

    app v1

--- Kore/catalog/models.py
+++ Kore/catalog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Users(models.Model):
+class User(models.Model):
     """Creates a model with the name, kerberos, and year of graduation of user"""
     # Fields
     name = models.CharField(max_length=80, verbose_name='Your Name', help_text='Your Name')
@@ -15,6 +15,13 @@ class Users(models.Model):
     ALUM = 'AL'
     grad_year_choices = [(FRESHMAN, 'Freshman'), (SOPHOMORE, 'Sophomore'), (JUNIOR, 'Junior'), (SENIOR, 'Senior'), (SUPERSENIOR, 'Super Senior'), (ALUM, 'Alumni')]
     grad_year = models.CharField(max_length=2, choices=grad_year_choices, default=FRESHMAN,)
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this user."""
+        return reverse('user-detail', args=[str(self.id)])
+
 
 
 class Psolved(models.Model):   
@@ -22,11 +29,23 @@ class Psolved(models.Model):
     class_name = models.CharField(max_length=20, verbose_name='Class No. (e.g. 6.006)', help_text='Enter the class no. as per catalog.mit.edu')
     assign_name = models.IntegerField(verbose_name='Pset No. (e.g. 1)', help_text='Enter the problem set number')
     question_number = models.IntegerField(verbose_name='Question No. (e.g. 1)', help_text='Enter the question which you have solved')
-    students = models.ManyToManyField('Users')
+    students = models.ManyToManyField('User')
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.class_name + str(self.assign_name) + str(self.question_number)
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this psolved."""
+        return reverse('psolved-detail', args=[str(self.id)])
 
 class Punsolved(models.Model):   
     'Creates a model with the class, assignment number, question number, and part number that user has not solved'
     class_name_u = models.CharField(max_length=20, verbose_name='Class No. (e.g. 6.006)', help_text='Enter the class no. as per catalog.mit.edu')
     assign_name_u = models.IntegerField(verbose_name='Pset No. (e.g. 1)', help_text='Enter the problem set number')
     question_number_u = models.IntegerField(verbose_name='Question No. (e.g. 1)', help_text='Enter the question which you have not solved')
-    students_u = models.ManyToManyField('Users')
+    students_u = models.ManyToManyField('User')
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.class_name + str(self.assign_name) + str(self.question_number)
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this punsolved."""
+        return reverse('user-detail', args=[str(self.id)])
