2188816ade453c109d9e203b089fbedd15a6ed00
Sagnik Anupam <sagnikanupam@gmail.com>
2020-09-20 00:33:52 +0530

    list view

--- Kore/catalog/models.py
+++ Kore/catalog/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
+class student(models.Model):
     """Creates a model with the name, kerberos, and year of graduation of user"""
     # Fields
-    name = models.CharField(max_length=80, verbose_name='Your Name', help_text='Your Name')
+    name = models.CharField(max_length=80, verbose_name='Name', help_text='Your Name')
     kerb = models.CharField(max_length = 8, verbose_name='Kerberos', primary_key = True, help_text='Your Kerberos ID')
     FRESHMAN = 'FR'
     SOPHOMORE = 'SO'
@@ -24,28 +24,40 @@ class User(models.Model):
 
 
 
-class Psolved(models.Model):   
+class solvedProblem(models.Model):   
     'Creates a model with the class, assignment number, and question number that user has solved'
-    class_name = models.CharField(max_length=20, verbose_name='Class No. (e.g. 6.006)', help_text='Enter the class no. as per catalog.mit.edu')
-    assign_name = models.IntegerField(verbose_name='Pset No. (e.g. 1)', help_text='Enter the problem set number')
-    question_number = models.IntegerField(verbose_name='Question No. (e.g. 1)', help_text='Enter the question which you have solved')
-    students = models.ManyToManyField('User')
+    class_name = models.CharField(max_length=20, verbose_name='Class No.', help_text='Enter the class no. as per catalog.mit.edu (e.g. 6.006)')
+    assign_name = models.IntegerField(verbose_name='Pset No.', help_text='Enter the problem set number (e.g. 1)')
+    question_number = models.IntegerField(verbose_name='Question No.', help_text='Enter the question which you have solved (e.g. 1)')
+    student_list = models.ManyToManyField('student')
     def __str__(self):
         """String for representing the Model object."""
         return self.class_name + str(self.assign_name) + str(self.question_number)
     def get_absolute_url(self):
         """Returns the url to access a detail record for this psolved."""
         return reverse('psolved-detail', args=[str(self.id)])
+    '''
+    def displayStudent(self):
+        """Create a string for the students. This is required to display students in Admin."""
+        return ', '.join(i.name for i in self.student.all()[:])
+    displayStudent.short_description = 'Students'
+    '''
 
-class UnsolvedP(models.Model):   
+class unsolvedProblem(models.Model):   
     'Creates a model with the class, assignment number, and question number that user has not solved'
-    class_name = models.CharField(max_length=20, verbose_name='Class No. (e.g. 6.006)', help_text='Enter the class no. as per catalog.mit.edu')
-    assign_name = models.IntegerField(verbose_name='Pset No. (e.g. 1)', help_text='Enter the problem set number')
-    question_number = models.IntegerField(verbose_name='Question No. (e.g. 1)', help_text='Enter the question which you have not solved')
-    students = models.ManyToManyField('User')
+    class_name = models.CharField(max_length=20, verbose_name='Class No.', help_text='Enter the class no. as per catalog.mit.edu (e.g. 6.006)')
+    assign_name = models.IntegerField(verbose_name='Pset No.', help_text='Enter the problem set number (e.g. 1)')
+    question_number = models.IntegerField(verbose_name='Question No.', help_text='Enter the question which you have not solved (e.g. 1)')
+    student_list = models.ManyToManyField('student')
     def __str__(self):
         """String for representing the Model object."""
         return self.class_name + str(self.assign_name) + str(self.question_number)
     def get_absolute_url(self):
         """Returns the url to access a detail record for this psolved."""
         return reverse('psolved-detail', args=[str(self.id)])
+    '''
+    def displayStudent(self):
+        """Create a string for the students. This is required to display students in Admin."""
+        return ', '.join(i.name for i in self.student.all()[:])
+    displayStudent.short_description = 'Students'
+    '''
